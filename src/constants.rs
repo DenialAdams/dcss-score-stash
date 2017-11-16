e5724527e544d932d14bc0b75bfe3abfaf3c9c10
Richard McCormack <brick@brick.codes>
2017-11-16 01:28:19 -0500

    Add important constants module

---
+++ src/constants.rs
@@ -0,0 +1,194 @@
+use models::NewSpecies;
+pub const SPECIES: [NewSpecies; 38] = [
+    NewSpecies {
+        short: "Ba",
+        name: "Barachi",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Ce",
+        name: "Centaur",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "DD",
+        name: "Deep Dwarf",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "DE",
+        name: "Deep Elf",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Dg",
+        name: "Demigod",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Dr",
+        name: "Draconian",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Ds",
+        name: "Demonspawn",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Fe",
+        name: "Felid",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Fo",
+        name: "Formicid",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Gh",
+        name: "Ghoul",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Gn",
+        name: "Gnoll",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Gr",
+        name: "Gargoyle",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "HE",
+        name: "High Elf",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "HO",
+        name: "Hill Orc",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Ha",
+        name: "Halfling",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Hu",
+        name: "Human",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Ko",
+        name: "Kobold",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Mf",
+        name: "Merfolk",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Mi",
+        name: "Minotaur",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Mu",
+        name: "Mummy",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Na",
+        name: "Naga",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Op",
+        name: "Octopode",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Og",
+        name: "Ogre",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Sp",
+        name: "Spriggan",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Te",
+        name: "Tengu",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Tr",
+        name: "Troll",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "VS",
+        name: "Vine Stalker",
+        playable: 1,
+    },
+    NewSpecies {
+        short: "Vp",
+        name: "Vampire",
+        playable: 1,
+    },
+    // non-playable
+    NewSpecies {
+        short: "El",
+        name: "Elf",
+        playable: 0,
+    },
+    NewSpecies {
+        short: "Gm",
+        name: "Gnome",
+        playable: 0,
+    },
+    NewSpecies {
+        short: "OM",
+        name: "Ogre-Mage",
+        playable: 0,
+    },
+    NewSpecies {
+        short: "HD",
+        name: "Hill Dwarf",
+        playable: 0,
+    },
+    NewSpecies {
+        short: "MD",
+        name: "Mountain Dwarf",
+        playable: 0,
+    },
+    NewSpecies {
+        short: "GE",
+        name: "Grey Elf",
+        playable: 0,
+    },
+    NewSpecies {
+        short: "SE",
+        name: "Sludge Elf",
+        playable: 0,
+    },
+    NewSpecies {
+        short: "LO",
+        name: "Lava Orc",
+        playable: 0,
+    },
+    NewSpecies {
+        short: "Dj",
+        name: "Djinni",
+        playable: 0,
+    },
+    NewSpecies {
+        short: "Pl",
+        name: "Plutonian",
+        playable: 0,
+    },
+];
